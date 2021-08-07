c1e5674c64a1e5e72eafff217907af5d55b793de
vtejaeta <vtejaeta@gmail.com>
2021-08-07 18:10:50 +0530

    Add stack problem solution for calculating minimum of stack

--- stacks_data_structures/index.js
+++ stacks_data_structures/index.js
@@ -1,6 +1,6 @@
 // Stack is a linear data structure in which addition or removal of elements follow a particular order i.e. LIFO(Last in First Out) AND FILO(First in Last Out).
 
-// Functions: push(), pop(), peek(), length()
+// Functions: push(), pop(), peek(), size()
 function myStack() {
   this.collection = {}
   this.count = 0
@@ -22,6 +22,7 @@ function myStack() {
     if (this.count == 0) {
       return undefined
     }
+
     this.count--
     let result = this.collection[this.count]
     delete this.collection[this.count]
@@ -42,3 +43,72 @@ console.log(newStack.peek())
 console.log(newStack.size())
 console.log(newStack.pop())
 console.log(newStack.peek())
+
+
+// Question - 1
+// Design a stack which, in addition to push and pop, has a function min which returns minimum element? Push, pop and min should all operate in O(1) time.
+
+function Stack() {
+  let collection = [];
+  let minArray = [];
+  let length = 0;
+
+  this.push = function (item) {
+    if(minArray.length == 0 || item <= this.min()){
+      minArray.push(item);
+    }
+    collection.push(item);
+    length++;
+
+    return length;
+  };
+
+  this.pop = function () {
+    if (length == 0) throw new Error(`Stack is empty`);
+    let poppedElement = collection.pop();
+    if(this.min() == poppedElement) minArray.pop();
+    length--;
+
+    return poppedElement;
+  };
+
+  this.peek = function () {
+    if (length == 0) throw new Error(`Stack is empty`);
+    return collection[length - 1];
+  };
+
+  this.isEmpty = function () {
+    return !length;
+  };
+
+  this.entries = function () {
+    return collection;
+  };
+
+  this.size = function () {
+    return length;
+  };
+
+  this.min = function () {
+    if(!minArray.length) throw new Error(`Stack is empty`);
+    return minArray[minArray.length-1];
+  };
+}
+
+let stack = new Stack();
+stack.push(5);
+stack.push(2);
+stack.push(1);
+stack.push(3);
+stack.push(6);
+stack.push(7);
+stack.push(1);
+stack.push(9);
+stack.min(); // 1
+stack.entries(); // [5,2,1,3,6,7,1,9]
+stack.pop(); // 9
+stack.min(); // 1
+stack.pop(); // 1
+stack.min(); // 1
+console.log(stack.entries());
+console.log(stack.min());
